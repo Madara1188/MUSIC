bc8034f6be0f904a40b315b2dd35b39f6c4ca857
Madara <147428837+Madara1188@users.noreply.github.com>
2024-04-11 20:27:13 +0530

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -72,7 +72,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.one.join_chat("DST_BOT_UPDATES")
+                await self.one.join_chat("Wynk_Music_Updates")
                 await self.one.join_chat("TEAM_DST")
             except:
                 pass
@@ -93,7 +93,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.one.join_chat("DST_BOT_UPDATES")
+                await self.one.join_chat("Wynk_Music_Updates")
                 await self.one.join_chat("TEAM_DST")
             except:
                 pass
@@ -115,7 +115,7 @@ class Userbot(Client):
             await self.four.start()
             try:
                
-                await self.one.join_chat("DST_BOT_UPDATES")
+                await self.one.join_chat("Wynk_Music_Updates")
                 await self.one.join_chat("TEAM_DST")
             except:
                 pass
@@ -137,7 +137,7 @@ class Userbot(Client):
             await self.five.start()
             try:
                 
-                await self.one.join_chat("DST_BOT_UPDATES")
+                await self.one.join_chat("Wynk_Music_Updates")
                 await self.one.join_chat("TEAM_DST")
             except:
                 pass
